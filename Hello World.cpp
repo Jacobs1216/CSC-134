4072783d2e32514e747693cabc6a955d2fc029cf
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-01-20 22:59:40 +0000

    Finished m1t1

--- Hello World.cpp
+++ Hello World.cpp
@@ -10,5 +10,12 @@ Hello, World M1T1
 using namespace std;
 
 int main() {
+    // Greet the user
+    cout << "hello, user!" << endl;
+    cout << "What's your name?" << endl;
+    string name;
+    cin >> name;
+    cout << "Nice to meet you, " << name << endl;
+    
     return 0;
 }
