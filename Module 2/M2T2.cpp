b747adc88759a97dda9f612f892844a7b0717733
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-02-23 01:49:50 +0000

    Save 1

---
+++ Module 2/M2T2.cpp
@@ -0,0 +1,15 @@
+/*
+CSC-134
+M2T2 Receipt Calculator
+Jacob Simmons
+2/22/2026
+*/
+
+
+#include <iostream>
+#include <iomanip> // for 2 decimal places
+using namespace std;
+
+int main() {
+    
+}
