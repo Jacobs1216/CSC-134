ed0ea3abec6cfa6376e257f2bbd863e03fcca3c9
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-02-23 23:27:51 +0000

    finished M2T2

--- Module 2/M2T2.cpp
+++ Module 2/M2T2.cpp
@@ -11,5 +11,27 @@ Jacob Simmons
 using namespace std;
 
 int main() {
-    
+    //purpose: create a receipt
+
+    //declare variables
+    string item = "cheeseburger";
+    double item_price = 5.99;
+    double tax_percent = 0.08; // 8% tax
+    double tax_amount;         // tax total in $
+    double total;              // price + tax
+
+    //great user an take order
+    cout << "Welcome to our restuarant!" << endl;
+    cout << "You ordered one " << item << "." << endl;
+
+    // calculate meal price
+    // calculate sales tax and total price
+    tax_amount = item_price * tax_percent;
+    total = item_price + tax_amount;
+
+    //print receipt
+    cout << setprecision(2) << fixed;
+    cout << "Your total is, " << total << endl;
+
+    return 0; // no errors
 }
