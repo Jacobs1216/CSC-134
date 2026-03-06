ae7effea67a6e6aa91a57d0a6aaa0157859f42c9
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-03-06 21:25:08 +0000

    update 1

---
+++ Module 2/M2LAB1.cpp
@@ -0,0 +1,37 @@
+/*
+Jacob Simmons
+M2LAB
+3/6/2026
+csc-134
+*/
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    //constants
+    double COST_PER_CUBIC_FOOT = 0.23;
+    double CHARGE_PER_CUBIC_FOOT = 0.5;
+
+    //variables
+    double length;
+    double width;
+    double height;
+    double volume;
+    double cost;
+    double charge;
+    double profit;
+
+    //input for crate dimensions
+    cout << "Enter the dimensions for the crate (in feet)" << endl;
+    cout << "input length" << endl;
+    cin >> lenght;
+    cout << "input width" << endl;
+    cin >> width;
+    cout << "input height" << endl;
+    cin >> height;
+    
+
+}
+
+}
