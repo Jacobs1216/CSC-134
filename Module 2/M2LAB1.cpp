74741c395451915bb12678e0abb08e6e91759fea
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-03-06 21:53:23 +0000

    Finsished M2LAB

--- Module 2/M2LAB1.cpp
+++ Module 2/M2LAB1.cpp
@@ -6,6 +6,7 @@ csc-134
 */
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main (){
@@ -22,6 +23,8 @@ int main (){
     double charge;
     double profit;
 
+    cout << setprecision(2) << fixed << showpoint;
+
     //input for crate dimensions
     cout << "Enter the dimensions for the crate (in feet)" << endl;
     cout << "input length" << endl;
@@ -31,5 +34,16 @@ int main (){
     cout << "input height" << endl;
     cin >> height;
     
+    //calculations
+    volume = length * width * height;
+    cost = volume * COST_PER_CUBIC_FOOT;
+    charge = volume * CHARGE_PER_CUBIC_FOOT;
+    profit = charge - cost;
 
+    //display of calculations
+    cout << "The volume of the crate is " << volume << " cubic feet" << endl;
+    cout << "The cost to build: $" << cost << endl;
+    cout << "Customer charge: $" << charge << endl;
+    cout << "Profit: $" << profit << endl;
+    return 0;
 }
