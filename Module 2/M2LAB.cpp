d52a14bb438abf5e4c5ff5eb3511428ea084814c
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-03-06 21:13:52 +0000

    m2lab update 1

---
+++ Module 2/M2LAB.cpp
@@ -0,0 +1,36 @@
+/*
+Jacob Simmons
+3/4/2026
+csc-134
+M2LAB
+calculator for volume and cost to build a crate
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //constants
+    double COST_PER_CUBIC_FOOT = 0.23;
+    double CHARGE_PER_CUBIC_FOOT = 0.5;
+
+    //variables
+    double length;
+    double width;
+    double height;
+    double volume;
+    double cost;
+    double charge;
+    double profit;
+
+    //input for crate dimensions
+    cout << "Enter the dimensions for the crate (in feet)" << endl;
+    cout << "input length" << endl;
+    cin >> lenght;
+    cout << "input width" << endl;
+    cin >> width;
+    cout << "input height" << endl;
+    cin >> height;
+    
+
+}
