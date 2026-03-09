600c3c625d77f3b4d28b494d66b1088ece2046fe
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-03-09 23:22:37 +0000

    finshed m2hwq1

---
+++ Module 2/M2HWQ1.cpp
@@ -0,0 +1,45 @@
+/*
+Jacob Simmons
+3/9/26
+csc-134
+M2HWQ1 - BANK ACCOUNT
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    
+    cout << setpresicion(2) << fixed << endl;
+
+    //variables
+    int accountNumber = 683654;
+    double startingBalance = 2656.53
+    double finalBalance;
+    double deposit;
+    double withdrawal;
+
+    //user input
+    cout << "Thank you for choosing City Bank, please enter your name." << endl;
+    cin >> name;
+
+    cout << "Hello, " << name << " your account balance is $" << startingBalance << endl;
+    cout << "enter your deposit amount" << endl;
+    cin >> deposit;
+    cout << "enter your withdrawal amount" << endl;
+    cin >> withdrawal;
+
+    //calculate final balance
+    finalBalance = startingBalance + deposit - withdrawal
+
+    //display results
+    cout << "------Account Statement------" << endl;
+    cout << "Owner Name: " << name << endl;
+    cout << "Account number: " << accountNumber << endl;
+    cout << "Account Balance: " << finalBalance << endl;
+
+    return 0;
+
+}
