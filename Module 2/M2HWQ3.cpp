7c9ac52f7b214423e764adec52d4d3b0b15c010b
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-03-10 03:59:21 +0000

    finished Q3

--- Module 2/M2HWQ3.cpp
+++ Module 2/M2HWQ3.cpp
@@ -16,8 +16,26 @@ int main () {
     int pizza;
     int slicePerPizza;
     int visitors;
+    int slicesEaten;
     int totalSlices;
     int leftoverSlices;
 
+    //user inputs
+    cout << "how many pizzas were ordered" << endl;
+    cin >> pizza;
 
+    cout << "How many slice are there per pizza?" << endl;
+    cin >> slicePerPizza;
+
+    cout << "How many visitors will there be?" << endl;
+    cin >> visitors;
+
+    //calculations
+    totalSlices = pizza * slicePerPizza;
+    slicesEaten = visitors * 3;
+    leftoverSlices = totalSlices - slicesEaten;
+
+    //display output
+    cout << "There are a " << totalSlices << " slices of pizza" << endl;
+    cout << "there are " << visitors << ", there will be " << leftoverSlices << " slices leftover." << endl;
 }
