dd55459999b98c0f77726abcd9bc3366d86b7415
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-03-10 03:26:52 +0000

    started Q3

---
+++ Module 2/M2HWQ3.cpp
@@ -0,0 +1,23 @@
+/*
+Jacob Simmons
+3/9/26
+csc-134
+M2HWQ3 - Pizza Party
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main () {
+
+    //variables
+    int pizza;
+    int slicePerPizza;
+    int visitors;
+    int totalSlices;
+    int leftoverSlices;
+
+
+}
