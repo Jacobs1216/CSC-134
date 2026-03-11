61ecf1349c61fe5565ebf7b439df1b5b76c26ff8
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-03-11 23:31:51 +0000

    finished q4

---
+++ Module 2/M2HWQ4.cpp
@@ -0,0 +1,27 @@
+/*
+Jacob Simmons
+3/11/26
+csc-134
+HWQ4 - cheer team
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string school, team;
+
+    cout << "insert school name" << endl;
+    cin >> school;
+    cout << "insert team name" << endl;
+    cin >> team;
+
+    //display output
+    cout << "Let's go " << school << endl;
+    cout << "Let's go " << school << endl;
+    cout << "Let's go " << school << endl;
+    cout << "Let's go " << team << endl;
+
+    return 0;
+    
+}
