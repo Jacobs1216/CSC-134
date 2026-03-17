21d528e61623d2f60eed700256855958e2ceecea
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-03-17 21:39:14 +0000

    finished m3lab2

--- Module 3/M3LAB2.cpp
+++ Module 3/M3LAB2.cpp
@@ -9,6 +9,38 @@ m3lab2
 using namespace std;
 
 int main () {
-    cout << "hello world" << endl;
+    
+    //variable
+    int grade;
+
+    cout << "Enter a numerical grade: " << endl;
+    cin >> grade;
+
+    if (grade >= 90 && grade <= 100)
+    {
+        cout << "Letter Grade: A" << endl;
+    }
+    else if (grade >= 80 && grade <= 89)
+    {
+        cout << "Letter Grade: B" << endl;
+    }
+    else if (grade >= 70 && grade <= 79)
+    {
+        cout << "Letter Grade: C" << endl;
+    }
+    else if (grade >= 60 && grade <= 69)
+    {
+        cout << "Letter Grade: D" << endl;
+    }
+    else if (grade >= 0 && grade <= 59)
+    {
+        cout << "Letter Grade: F" << endl;
+    }
+    else
+    {
+        cout << "Invalid grade entered." << endl;
+    }
+
+    return 0;
 
 }
