a39addebf50fb2b9a3896d31e0460e6f02123a82
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-03-17 21:01:40 +0000

    started m3lab1

---
+++ Module 3/M3LAB1.cpp
@@ -0,0 +1,26 @@
+/*
+Jacob Simmons
+3/17/2026
+csc-134
+m3lab1
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int choice;
+
+    //question
+    cout << "You're a Nascar driver in 1st place" << endl;
+    cout << "Your car begin to make a weird noise." << endl;
+    cout << "What do you do?" << endl;
+    cout << "1. Ignore the noise and keep racing and hold your current postion." << endl;
+    cout << "2. Go into the pit lane and investigate, and risk losing 1st place." << endl;
+
+
+
+
+}
