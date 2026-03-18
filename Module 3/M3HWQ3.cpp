f00af17f839bb8fe00575dbd588350423de283f0
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-03-18 23:53:44 +0000

    update 1

--- Module 3/M3HWQ3.cpp
+++ Module 3/M3HWQ3.cpp
@@ -20,7 +20,24 @@ int main() {
     cout << "(You may lose current position from pitting and will have a slower top speed)" << endl;
     cout << "Choice 2: Continue racing on standard racing slicks" << endl;
     cout << "(You will be faster but you have a higher chance losing traction and crashing)" << endl;
-    cout << "Please enter 1 or 2 to choose" << endl;
+    cout << "To choose enter 1 or 2" << endl;
     cin >> choice;
+
+    //CHOICE 1
+    if (choice == "1") {
+
+        string choice1;
+
+        cout << "Everyone else decided to change tires aswell so you hold current postion" << endl;
+        cout << "Now youre in 2nd place in a battle for first how do you handle the last turn before the finsh line?" << endl;
+        cout << "choice 1: try to out break your opponent and overtake in the corner" << endl;
+        cout << "Choise 2: wait until after the corner and try to pass in the straight right before the finsih line" << endl;
+        cin >> choice1;
+
+    }
     
+
+
+
+    return 0;
 }
