5be39ae39edddd76ac30648c2e857a1e3f6173b9
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-03-18 23:15:36 +0000

    started q3

---
+++ Module 3/M3HWQ3.cpp
@@ -0,0 +1,26 @@
+/*
+Jacob Simmons
+3/18/2026
+csc-134
+m3hwq3
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //variables
+    string choice;
+    
+    // PROMPT
+    cout << "You're a Formula 1 driver currently racing as it begins to rain." << endl;
+    cout << "Choice 1: Pit and change to wet weather tires." << endl;
+    cout << "(You may lose current position from pitting and will have a slower top speed)" << endl;
+    cout << "Choice 2: Continue racing on standard racing slicks" << endl;
+    cout << "(You will be faster but you have a higher chance losing traction and crashing)" << endl;
+    cout << "Please enter 1 or 2 to choose" << endl;
+    cin >> choice;
+    
+}
