def1f0c291ef2ebb0e5f95ff67958efdfdb6b24d
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-03-19 18:48:07 +0000

    update 3

--- Module 3/M3HWQ3.cpp
+++ Module 3/M3HWQ3.cpp
@@ -28,23 +28,39 @@ int main() {
 
         string choice1;
 
-        cout << "Everyone else decided to change tires aswell so you hold current postion" << endl;
+        cout << "Everyone else decided to change tires aswell so you hold current position" << endl;
         // 2ND QUESTION
-        cout << "Now youre in 2nd place in a battle for first how do you handle the last turn before the finsh line?" << endl;
-        cout << "choice 1: try to out break your opponent and overtake in the corner" << endl;
-        cout << "Choice 2: wait until after the corner and try to pass in the straight right before the finsih line" << endl;
+        cout << "Now youre in 2nd place in a battle for first how do you handle the last turn before the finish line?" << endl;
+        cout << "choice 1: try to out brake your opponent and overtake in the corner" << endl;
+        cout << "Choice 2: wait until after the corner and try to pass in the straight right before the finish line" << endl;
         cin >> choice1;
 
-    }
+        if (choice1 == "1") {
+            cout << "You dive down the inside and out-brake your opponent" << endl;
+            cout << "You take the lead and win the race" << endl;
+            }
+        else if (choice1 == "2") {
+            cout << "You wait for the straight and go for a clean overtake" << endl;
+            cout << "But your opponent defends well and you finish 2nd" << endl;
+        }
+        
+        else {
+            cout << "Invalid choice. You lose focus and miss your chance." << endl;
+        }
+
+        }
     
     else if (choice == "2") {
 
         cout << "The rain continues on throughout the race" << endl;
         cout << "You hold P1 for awhile you eventually make a mistake and spin out off the track taking you out of the race." << endl;
-        cout << "GAMEOVER" << endl;
+        cout << "YOU LOSE" << endl;
 
     }
 
+    else {
+        cout << "invalid choice" << endl;
+    }
 
     return 0;
 }
