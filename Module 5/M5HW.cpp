b58b4b6385013e82582ecb626b20d3a5310ac3db
Jacobs1216 <simmonsj0423@student.faytechcc.edu>
2026-04-15 23:43:11 +0000

    m5hw update 1

---
+++ Module 5/M5HW.cpp
@@ -0,0 +1,52 @@
+/*
+Jacob Simmons
+csc-134
+4/15/2026
+m5hw
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+//fucntions for each program
+void rainfall();
+void volumeCalculator();
+void romanNumeral();
+void areaCalculator();
+void distanceCalculator();
+
+void rainfall() {
+    string month1, month2, month3
+    double rain1, rain2, rain3
+
+    cout << "enter month: " << endl;
+    cin >> month1
+    cout << "enter rainfall for " << month1 << endl;
+    cin >> rain1
+
+    cout << "enter month: " << endl;
+    cin >> month2
+    cout << "enter rainfall for " << month1 << endl;
+    cin >> rain2
+
+    cout << "enter month: " << endl;
+    cin >> month3
+    cout << "enter rainfall for " << month1 << endl;
+    cin >> rain3
+
+    double avg = (r1 + r2 + r3) / 3.0;
+
+    cout << fixed << setprecision(2)
+    cout << "The average rainfall for " << month1 << ", " << month2 << ", and" << month3
+         << "is " << avg << " inches." << endl;
+
+return 0
+}
+
+void volumeCalculator() {
+
+}
+
+
